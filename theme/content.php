ab34a2a78077e2138d12312e3f2c386b6f3b2d84
Gus Perez <gus@gusperez.com>
2016-10-15 17:16:01 -0700

    Fixed up support for post thumbnails so I stop having to add them explicitly to body of posts

--- theme/content.php
+++ theme/content.php
@@ -14,22 +14,25 @@
 	<header class="entry-header">
 		<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
 		<div class="entry-thumbnail">
-			<?php the_post_thumbnail(); ?>
+			<?php the_post_thumbnail($size='175px'); ?>
 		</div>
 		<?php endif; ?>
 
-		<?php if ( is_single() ) : ?>
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-		<?php else : ?>
-		<h1 class="entry-title">
-			<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-		</h1>
-		<?php endif; // is_single() ?>
+		<div class="entry-headline">
+			<?php if ( is_single() ) : ?>
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+			<?php else : ?>
+				<h1 class="entry-title">
+					<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+				</h1>
+			<?php endif; // is_single() ?>
 
-		<div class="entry-meta">
-			<?php twentythirteen_entry_meta(); ?>
-			<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
-		</div><!-- .entry-meta -->
+			<div class="entry-meta">
+				<?php twentythirteen_entry_meta(); ?>
+				<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
+			</div><!-- .entry-meta -->
+		</div>
+
 	</header><!-- .entry-header -->
 
 	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
